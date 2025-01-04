bbdbe9dbc1226128a77d2a8c336b61888d471d75
Aziz Rahmad <azayrahmad@gmail.com>
2025-01-05 00:03:48 +0800

    refactor agent

---
+++ ClippitWinforms/Agent.cs
@@ -0,0 +1,133 @@
+﻿namespace ClippitWinforms;
+
+public class Agent : IDisposable
+{
+    private AnimationManager animationManager;
+    private AudioManager audioManager;
+    private StateManager stateManager;
+    private Balloon speechBalloon;
+
+    public event EventHandler? FrameChanged;
+    public event EventHandler<string>? AnimationCompleted;
+
+    public Agent(Form parentForm, string spritePath, string animationJsonPath,
+        string soundsJsonPath, string stateJsonPath)
+    {
+        InitializeManagers(spritePath, animationJsonPath, soundsJsonPath, stateJsonPath);
+
+        // Create the speech balloon
+        speechBalloon = new Balloon(parentForm);
+    }
+
+    private void InitializeManagers(string spritePath, string animationJsonPath,
+        string soundsJsonPath, string stateJsonPath)
+    {
+        // Create sprite manager with transparency key
+        var spriteManager = new BitmapSpriteManager(
+            spritePath,
+            124, // sprite width
+            93,  // sprite height
+            Color.FromArgb(255, 0, 255) // transparency key
+        );
+
+        animationManager = new AnimationManager(spriteManager, animationJsonPath);
+        audioManager = new AudioManager(soundsJsonPath);
+
+        animationManager.FrameChanged += (s, e) => {
+            var currentFrame = animationManager.GetCurrentFrame();
+            if (currentFrame?.Sound != null)
+            {
+                audioManager.PlayFrameSound(currentFrame.Sound);
+            }
+            FrameChanged?.Invoke(this, EventArgs.Empty);
+        };
+
+        animationManager.AnimationCompleted += async (s, animName) => {
+            AnimationCompleted?.Invoke(this, animName);
+            await HandleAnimationCompleted();
+        };
+
+        stateManager = new StateManager(stateJsonPath, animationManager);
+    }
+
+    public IEnumerable<string> GetSelectableAnimations()
+    {
+        return animationManager.GetSelectableAnimations().OrderBy(a => a);
+    }
+
+    public IEnumerable<string> GetAvailableStates()
+    {
+        return stateManager.GetAvailableStates();
+    }
+
+    public async Task PlayStartupAnimation()
+    {
+        await animationManager.PlayAnimation("Greeting");
+        speechBalloon.ShowBalloon(
+            "Hello",
+            "It looks like you're trying to write a letter.\r\rWould you like help?",
+            10000);
+        await stateManager.SetState("IdlingLevel1");
+    }
+
+    public async Task PlayClosingAnimation()
+    {
+        await animationManager.InterruptAndPlayAnimation("GoodBye");
+    }
+
+    public async Task PlayAnimation(string animationName, int? timeoutMs = null)
+    {
+        await stateManager.PlayAnimationOnce(animationName, timeoutMs);
+    }
+
+    public async Task SetState(string state)
+    {
+        await stateManager.SetState(state);
+    }
+
+    public async Task PlayRandomAnimation()
+    {
+        await stateManager.PlayRandomAnimation();
+    }
+
+    private async Task HandleAnimationCompleted()
+    {
+        await stateManager.HandleAnimationCompleted();
+    }
+
+    public async Task HandleVisibilityChange(bool isVisible)
+    {
+        await stateManager.HandleVisibilityChange(isVisible);
+        if (!isVisible)
+        {
+            speechBalloon.HideBalloon();
+        }
+    }
+
+    public void DrawCurrentFrame(Graphics graphics)
+    {
+        animationManager.DrawCurrentFrame(graphics);
+    }
+
+    public string GetCurrentState()
+    {
+        return stateManager.GetCurrentState();
+    }
+
+    public string GetCurrentAnimationName()
+    {
+        return animationManager.CurrentAnimation.Name;
+    }
+
+    public int GetCurrentFrameIndex()
+    {
+        return animationManager.CurrentFrameIndex;
+    }
+
+    public void Dispose()
+    {
+        audioManager.Dispose();
+        animationManager.Dispose();
+        stateManager.Dispose();
+    }
+}
--- ClippitWinforms/CharacterView.cs
+++ ClippitWinforms/CharacterView.cs
@@ -1,61 +1,27 @@
-using System.Drawing.Imaging;
-using System.Text.Json;
+using System.Drawing;
 
 namespace ClippitWinforms
 {
     public partial class CharacterView : Form
     {
-        private AnimationManager animationManager;
-        private AudioManager audioManager;
-        private StateManager stateManager;
+        private Agent agent;
         private bool isClosing = false;
-        private IEnumerable<string> Animations;
-        private Balloon speechBalloon;
-
         private Point lastPoint;
 
         public CharacterView()
         {
             InitializeComponent();
             InitializePosition();
-            InitializeManagers();
+            InitializeAgent();
             InitializeSelectionMenu();
-            PlayStartupAnimation();
 
-            // Create the speech balloon
-            speechBalloon = new Balloon(this);
-
-            //// Load settings from JSON
-            //string jsonSettings = @"{
-            //    ""Balloon"": [
-            //        {
-            //            ""NumLines"": 2,
-            //            ""CharsPerLine"": 28,
-            //            ""FontName"": ""MS W98 UI"",
-            //            ""FontHeight"": 13,
-            //            ""ForeColor"": ""00000000"",
-            //            ""BackColor"": ""00e1ffff"",
-            //            ""BorderColor"": ""00000000""
-            //        }
-            //    ]
-            //}";
-
-            //// Extract the first balloon settings
-            //using (JsonDocument doc = JsonDocument.Parse(jsonSettings))
-            //{
-            //    JsonElement firstBalloon = doc.RootElement.GetProperty("Balloon")[0];
-            //    speechBalloon.LoadSettings(firstBalloon.ToString());
-            //}
-
+            agent.PlayStartupAnimation();
         }
 
-        #region Initializations
         private void InitializePosition()
         {
             this.StartPosition = FormStartPosition.Manual;
             Rectangle workingArea = Screen.GetWorkingArea(this);
-
-            // Agent is positioned on bottom right of the screen
             this.Location = new Point(workingArea.Right - this.Width, workingArea.Bottom - this.Height);
 
             // Enable dragging
@@ -63,61 +29,35 @@ namespace ClippitWinforms
             this.MouseMove += OnMouseMove;
         }
 
-        private void InitializeSelectionMenu()
-        {
-            // Populate Select Animation
-            Animations = animationManager.GetSelectableAnimations().OrderBy(a => a);
-            foreach (var animation in Animations)
-            {
-                var menuItem = new ToolStripMenuItem(animation);
-                menuItem.Click += async (sender, e) => await PlaySelectedAnimation(animation);
-                selectAnimationToolStripMenuItem.DropDownItems.Add(menuItem);
-            }
-
-            // Populate Select State
-            var availableStates = stateManager.GetAvailableStates();
-            foreach (var state in availableStates)
-            {
-                var menuItem = new ToolStripMenuItem(state);
-                menuItem.Click += async (sender, e) => await stateManager.SetState(state);
-                selectStateToolStripMenuItem.DropDownItems.Add(menuItem);
-            }
-        }
-
-        private void InitializeManagers()
+        private void InitializeAgent()
         {
             string spritePath = @"D:\Exercises\ClippitWinforms\ClippitWinforms\map.png";
             string animationJsonPath = @"C:\Users\azayr\OneDrive\Documents\GitHub\ClippitWinforms\ClippitWinforms\animation.json";
             string soundsJsonPath = @"C:\Users\azayr\OneDrive\Documents\GitHub\ClippitWinforms\ClippitWinforms\sounds-mp3.json";
             string stateJsonPath = @"C:\Users\azayr\OneDrive\Documents\GitHub\ClippitWinforms\ClippitWinforms\states.json";
 
-            // Create sprite manager with transparency key
-            var spriteManager = new BitmapSpriteManager(
-                spritePath,
-                124, // sprite width
-                93,  // sprite height
-                Color.FromArgb(255, 0, 255) // transparency key
-            );
-
-            animationManager = new AnimationManager(spriteManager, animationJsonPath);
-            audioManager = new AudioManager(soundsJsonPath);
-
-            animationManager.FrameChanged += AnimationManager_FrameChanged;
-            animationManager.AnimationCompleted += AnimationManager_AnimationCompleted;
-            stateManager = new StateManager(stateJsonPath, animationManager);
+            agent = new Agent(this, spritePath, animationJsonPath, soundsJsonPath, stateJsonPath);
+            agent.FrameChanged += (s, e) => this.Invalidate();
         }
-        #endregion
 
-        private void AnimationManager_FrameChanged(object sender, EventArgs e)
+        private void InitializeSelectionMenu()
         {
-            var currentFrame = animationManager.GetCurrentFrame();
-            if (currentFrame?.Sound != null)
+            // Populate Select Animation
+            foreach (var animation in agent.GetSelectableAnimations())
             {
-                audioManager.PlayFrameSound(currentFrame.Sound);
+                var menuItem = new ToolStripMenuItem(animation);
+                menuItem.Click += async (sender, e) => await agent.PlayAnimation(animation);
+                selectAnimationToolStripMenuItem.DropDownItems.Add(menuItem);
             }
-            this.Invalidate();
-        }
 
+            // Populate Select State
+            foreach (var state in agent.GetAvailableStates())
+            {
+                var menuItem = new ToolStripMenuItem(state);
+                menuItem.Click += async (sender, e) => await agent.SetState(state);
+                selectStateToolStripMenuItem.DropDownItems.Add(menuItem);
+            }
+        }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -130,58 +70,19 @@ namespace ClippitWinforms
             else Show();
         }
 
-        #region Graphics
-
-        private async Task PlayStartupAnimation()
-        {
-            await animationManager.PlayAnimation("Greeting");
-            // Show the balloon
-            speechBalloon.ShowBalloon(
-                "Hello",
-                "It looks like you're trying to write a letter.\r\rWould you like help?", 10000);
-
-            await stateManager.SetState("IdlingLevel1"); // Set initial state instead of direct animation
-        }
-
-        private async Task PlayClosingAnimation()
-        {
-            await animationManager.InterruptAndPlayAnimation("GoodBye");
-        }
-
-        private async Task PlaySelectedAnimation(string animationName, int? timeoutMs = null)
-        {
-            try
-            {
-                // Start continuous playback of the selected animation
-                // await stateManager.StartContinuousAnimation(animationName, timeoutMs);
-                // await animationManager.InterruptAndPlayAnimation(animationName);
-                await stateManager.PlayAnimationOnce(animationName, timeoutMs);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error playing animation: {ex.Message}");
-            }
-        }
-
-        private async void AnimationManager_AnimationCompleted(object sender, string animationName)
-        {
-            // Let the state manager handle animation completion
-            await stateManager.HandleAnimationCompleted();
-        }
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            animationManager.DrawCurrentFrame(e.Graphics);
+            agent.DrawCurrentFrame(e.Graphics);
 #if DEBUG
             // Debug information
-            string debugText = $"Frame: {animationManager.CurrentFrameIndex}";
             Font debugFont = new Font("Arial", 12, FontStyle.Bold);
-            e.Graphics.DrawString(debugText, debugFont, Brushes.White, new PointF(10, 10));
-            e.Graphics.DrawString($"State: {stateManager.GetCurrentState()}", debugFont, Brushes.White, new PointF(10, 25));
-            e.Graphics.DrawString($"Anim: {animationManager.CurrentAnimation.Name}", debugFont, Brushes.White, new PointF(10, 40));
+            e.Graphics.DrawString($"Frame: {agent.GetCurrentFrameIndex()}", debugFont, Brushes.White, new PointF(10, 10));
+            e.Graphics.DrawString($"State: {agent.GetCurrentState()}", debugFont, Brushes.White, new PointF(10, 25));
+            e.Graphics.DrawString($"Anim: {agent.GetCurrentAnimationName()}", debugFont, Brushes.White, new PointF(10, 40));
 #endif
         }
-        #endregion
+
         private void OnMouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
@@ -206,11 +107,8 @@ namespace ClippitWinforms
                 e.Cancel = true;
                 isClosing = true;
 
-                await PlayClosingAnimation();
-
-                audioManager.Dispose();
-                animationManager.Dispose();
-                stateManager.Dispose();
+                await agent.PlayClosingAnimation();
+                agent.Dispose();
                 trayIcon.Dispose();
 
                 Application.Exit();
@@ -223,21 +121,20 @@ namespace ClippitWinforms
             if (Visible)
             {
                 menu.Text = "Show";
-                await stateManager.HandleVisibilityChange(false);
+                await agent.HandleVisibilityChange(false);
                 Hide();
-                speechBalloon.HideBalloon();
             }
             else
             {
                 menu.Text = "Hide";
                 Show();
-                await stateManager.HandleVisibilityChange(true);
+                await agent.HandleVisibilityChange(true);
             }
         }
 
         private async void animateToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            await stateManager.PlayRandomAnimation();
+            await agent.PlayRandomAnimation();
         }
     }
 }
